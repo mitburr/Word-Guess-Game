6ffbc423da0e9f2f94aed76d08383a47e928166b
mitburr <mitburr@gmail.com>
2019-01-28 20:56:35 -0600

    more JS developments: core logic of the game pretty much finished. Just adding display for inputs now

--- stuff-n-things/javascript/game.js
+++ stuff-n-things/javascript/game.js
@@ -1,39 +1,46 @@
 let game = {
+  run: false,
   wins: 0,
   losses: 0,
   guesses: 0,
   lettersGuessed : [],
   wordsArrayIndex : 0,
   wordsArray : [
-    "butt",
+    "element",
     "hat",
-    "slut",
+    "bird",
   ],
 
-  start: function(key){
+  reset: function(){
     guesses = 0;
-    this.wordsArrayIndex++;
-    let marker = key;
-    console.log("test");
+    this.run = true;
   },
 
   guess: function(key){
     let word = this.wordsArray[this.wordsArrayIndex];
-    console.log(word);
     let lastLetterIndex = word.lastIndexOf(key);
-    let i = 0;
-    while (i != lastLetterIndex){
-      if (word[i] == guess){
-        console.log("test");
+    let i = 0; 
+    let correctLetters = 0;
+    while ((i != lastLetterIndex || word[i] == key) && i != (word.length)){
+      if (word[i] == key){
+        console.log("Correct letter");
+        correctLetters++;
       }
-      i ++
+      i ++;
+    }
+    this.guesses++;
+    if(guesses == 10){
+      const textDiv = document.getElementById("Hangman and/or text");
+      textDiv.innerHTML("<h1> you lost </h1>");
+      this.run == false;
     }
   },
 }
-// result = function () {
-//     wordHolder = document.getElementById('letters');
-//     correct = document.createElement('ul');
-//     correct.appendChild()
+result = function () {
+    wordHolder = document.getElementById('letters');
+    correct = document.createElement('ul');
+    correct.appendChild("test")
+}
 //     for (var i = 0; i < word.length; i++) {
 //       correct.setAttribute('id', 'my-word');
 //       guess = document.createElement('li');
@@ -50,13 +57,15 @@ let game = {
 //       correct.appendChild(guess);
 //     }
 //   }
+const clickHandler = function(event){
+  game.reset();
+}
 const keyHandler = function(event){
   const key = event.key;
-  if (key == 1){
-    game.start(key);
-  }
-  else{
+  if (game.run) {
     game.guess(key);
   }
 }
+let start = document.getElementById("start");
 document.onkeyup = keyHandler;
+start.onclick = clickHandler;
